402e9e9cd71bcdcaa155c6d058fa955ac11b4ec2
Victor Seidel <victorgabrielms88@gmail.com>
2025-03-21 17:41:58 -0300

    Creating SpringBoot project

---
+++ API/src/test/java/com/victor/pi3/Pi3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victor.pi3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pi3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
